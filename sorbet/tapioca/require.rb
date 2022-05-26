07007bf386ac2edbeabe90e4ed7aa91afbf1b736
蒼時弦也 <elct9620@frost.tw>
2022-05-26 18:53:51 +0800

    Setup tapioca for sorbet

---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,18 @@
+# typed: true
+# frozen_string_literal: true
+
+require 'active_support/core_ext/integer/time'
+require 'bootsnap/setup'
+require 'bundler/setup'
+require 'cucumber/rails'
+require 'cucumber'
+require 'factory_bot'
+require 'faker'
+require 'rack-mini-profiler'
+require 'rails/all'
+require 'rspec/rails'
+require 'shoulda'
+require 'shoulda/matchers'
+require 'simplecov'
+require 'simplecov-cobertura'
+require 'test_helper'
