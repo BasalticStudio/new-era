75e5c123f2a55c4fb5945c33e8e83aea663d16ee
蒼時弦也 <elct9620@frost.tw>
2022-05-26 19:47:29 +0800

    Setup flipper table

---
+++ config/initializers/flipper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'active_support/cache'
+require 'flipper/adapters/active_support_cache_store'
+
+Flipper.configure do |config|
+  config.adapter do
+    Flipper::Adapters::ActiveSupportCacheStore.new(
+      Flipper::Adapters::Memory.new,
+      ActiveSupport::Cache::MemoryStore.new, # Or Rails.cache
+      expires_in: 5.minutes
+    )
+  end
+end
