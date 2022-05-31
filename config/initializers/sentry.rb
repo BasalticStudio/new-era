9aa8c5ec612c6e771d7a6f9ba523a9683d9ef084
蒼時弦也 <elct9620@frost.tw>
2022-05-31 13:44:54 +0800

    Exclude health check for sentry performance tracking

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,21 @@
 
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.traces_sample_rate = 1.0
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /request/
+      case transaction_name
+      when %r{^/status}
+        0.0
+      else
+        1.0
+      end
+    else
+      1.0
+    end
+  end
 end
