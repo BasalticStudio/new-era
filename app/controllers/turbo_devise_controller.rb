d6803f4d2ed9c8aa50aa7271c2d940af31ca9bf8
蒼時弦也 <elct9620@frost.tw>
2022-06-07 21:03:12 +0800

    Improve TurboDeviseController#to_turbo_stream to more readable

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -7,12 +7,16 @@ class TurboDeviseController < ApplicationController
       controller.render(options.merge(formats: :html))
     rescue ActionView::MissingTemplate => e
       raise e if get?
-      return render_error if has_errors? && default_action
+      return unprocessable_entity if unprocessable?
 
       redirect_to navigation_location
     end
 
-    def render_error
+    def unprocessable?
+      has_errors? && default_action
+    end
+
+    def unprocessable_entity
       render rendering_options.merge(formats: :html,
                                      status: :unprocessable_entity)
     end
