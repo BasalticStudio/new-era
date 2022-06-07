de6a0806cfbfb4fd7768471ec6dab4ad6e70b182
蒼時弦也 <elct9620@frost.tw>
2022-06-07 17:50:44 +0800

    Setup view component rspec test helper

---
+++ spec/support/view_component.rb
@@ -0,0 +1,8 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'view_component/test_helpers'
+
+RSpec.configure do |config|
+  config.include ViewComponent::TestHelpers, type: :component
+end
