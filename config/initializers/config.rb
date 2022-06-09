dde20d4eafd6952bf2b0c4af490faa644db2144a
蒼時弦也 <elct9620@frost.tw>
2022-06-09 21:24:03 +0800

    Setup config gem

---
+++ config/initializers/config.rb
@@ -0,0 +1,10 @@
+# typed: false
+# frozen_string_literal: true
+
+Config.setup do |config|
+  config.const_name = 'Settings'
+  config.use_env = true
+  config.env_prefix = 'SETTINGS'
+  config.env_separator = '__'
+  config.env_parse_values = true
+end
