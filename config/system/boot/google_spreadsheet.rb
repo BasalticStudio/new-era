7eefc72a6bfa5b10dd8018bbd111205036bea3a9
蒼時弦也 <elct9620@frost.tw>
2022-09-05 21:25:24 +0800

    Let google spreadsheet service lazy load

--- config/system/boot/google_spreadsheet.rb
+++ config/system/boot/google_spreadsheet.rb
@@ -2,14 +2,16 @@
 # frozen_string_literal: true
 
 NewEra::Container.register_provider(:google_spreadsheet) do
-  start do
-    service = Google::Apis::SheetsV4::SheetsService.new
-    service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: Rails.root.join('config/credentials/spreadsheet-service_account.json'),
-      scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
-    )
-    service.authorization.fetch_access_token!
+  prepare do
+    register('google.spreadsheet') do
+      service = Google::Apis::SheetsV4::SheetsService.new
+      service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+        json_key_io: Rails.root.join('config/credentials/spreadsheet-service_account.json'),
+        scope: Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
+      )
 
-    register('google.spreadsheet', service)
+      service.authorization.fetch_access_token!
+      service
+    end
   end
 end
