1a675cbc272455d0a1f4717f843b51cfb1259e2e
蒼時弦也 <elct9620@frost.tw>
2022-12-08 14:34:32 +0800

    Fix rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join('/spec/fixtures')
 
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
